b56229b063198e62aa52ff539806760e24e4d3b8
omkar panchal <omkar13021@gmail.com>
2023-08-13 21:10:33 +0530

    added favicon

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const connectDB = require('./config/dbConnection');
 const errorHandler = require('./middleware/errorHandler');
 
 env.config(); //Configuring Hostname and Port from dotenv file
-const hostname = process.env.HOSTNAME;
+const hostname = process.env.HOSTNAME || 'localhost';
 const port = process.env.PORT || 5000;
 
 //Creating express app and hosting static file in public folder and connecting mongoDB
