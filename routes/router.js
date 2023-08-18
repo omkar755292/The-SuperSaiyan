7d29e52da1d234304e72a04b2c2ce159a3dfd018
root <omkar13021@gmail.com>
2023-08-18 13:16:30 +0000

    separted frontend and backend

--- routes/router.js
+++
@@ -1,9 +0,0 @@
-const exp = require('constants');
-const express= require('express');
-const postScore = require('../controller');
-
-const router = express.Router();
-
-router.route('/').post(postScore);
-
-module.exports = router;
--- server.js
+++ server.js
@@ -1,23 +1,14 @@
 const express = require('express');
 const env = require('dotenv');
 const path = require('path');
-const router = require('./routes/router');
-const connectDB = require('./config/dbConnection');
-const errorHandler = require('./middleware/errorHandler');
 
 env.config(); //Configuring Hostname and Port from dotenv file
 const hostname = process.env.HOSTNAME || 'localhost';
 const port = process.env.PORT || 5000;
 
 //Creating express app and hosting static file in public folder and connecting mongoDB
-connectDB();
 const app = express(); 
 app.use(express.static(path.join(__dirname, 'public')));
-
-//Setting up middleware and routes
-app.use(express.json());
-app.use('/api/score', router);
-app.use(errorHandler);
 
 //Starting a server on port
 app.listen(port, (req, res)=>{
